bbafb7e228acf3a565a7f3f75d12fee50605794f
apremizova <apremizova@edu.hse.ru>
2019-10-06 00:10:53 +0300

    Complete base form

--- CreditRisks/CreditRisks/Models/Company.cs
+++ CreditRisks/CreditRisks/Models/Company.cs
@@ -34,33 +34,32 @@ namespace CreditRisks.Models
         /// <summary>
         /// Риск бизнес-модели
         /// </summary>
-        [Range(0, 1)]
-        public float BusinessModelRisk { get; set; }
+        public int BusinessModelRisk { get; set; }
 
         /// <summary>
         /// Риск организацнонной структуры
         /// </summary>
-        public bool OrganizationStructureRisk { get; set; }
+        public int OrganizationStructureRisk { get; set; }
 
         /// <summary>
         /// Положительная информация по составу акционеров
         /// </summary>
-        public bool PositiveShareholders { get; set; }
+        public int PositiveShareholders { get; set; }
 
         /// <summary>
         /// Отрицательная информация по составу акционеров
         /// </summary>
-        public bool NegativeShareholders { get; set; }
+        public int NegativeShareholders { get; set; }
 
         /// <summary>
         /// Желание/возможность вкладывать дополнительные средства в бизнес
         /// </summary>
-        public bool DesireToInvest { get; set; }
+        public int DesireToInvest { get; set; }
 
         /// <summary>
         /// Изъятие собственником средств из бизнеса
         /// </summary>
-        public bool WithdrawalFunds { get; set; }
+        public int WithdrawalFunds { get; set; }
 
         /// <summary>
         /// Наличие споров по доле в собственном капитале или активам компании между собственниками
@@ -113,5 +112,79 @@ namespace CreditRisks.Models
         public bool RelevantRepayment { get; set; }
 
         // Данные бух учета
+        /// <summary>
+        /// Запасы
+        /// </summary>
+        public float Code_12103 { get; set;  }
+        
+        /// <summary>
+        /// Запасы - на 31 декабря предыдущего года
+        /// </summary>
+        public float Code_12104 { get; set;  }
+        
+        /// <summary>
+        /// Дебиторская задолженность
+        /// </summary>
+        public float Code_12303 { get; set;  }
+        
+        /// <summary>
+        /// Дебиторская задолженность - на 31 декабря предыдущего года
+        /// </summary>
+        public float Code_12304 { get; set;  }
+        
+        /// <summary>
+        /// Денежные средства и денежные эквиваленты
+        /// </summary>
+        public float Code_12503 { get; set;  }
+        
+        /// <summary>
+        /// Итого оборотных активов
+        /// </summary>
+        public float Code_12003 { get; set;  }
+        
+        /// <summary>
+        /// Баланс
+        /// </summary>
+        public float Code_16003 { get; set;  }
+        
+        /// <summary>
+        /// Итого капитала и резервов
+        /// </summary>
+        public float Code_13003 { get; set;  }
+        
+        /// <summary>
+        /// Итого долгосрочных обязательств
+        /// </summary>
+        public float Code_14003 { get; set;  }
+        
+        /// <summary>
+        /// Кредиторская задолженность
+        /// </summary>
+        public float Code_15203 { get; set;  }
+        
+        /// <summary>
+        /// Кредиторская задолженность - на 31 декабря предыдущего года
+        /// </summary>
+        public float Code_15204 { get; set;  }
+        
+        /// <summary>
+        /// Итого краткосрочных обязательств
+        /// </summary>
+        public float Code_15003 { get; set;  }
+        
+        /// <summary>
+        /// Чистая прибыль (убыток) - за последние 4 квартала
+        /// </summary>
+        public float Code_24003 { get; set;  }
+        
+        /// <summary>
+        /// Прибыль (убыток) от продаж - за последние 4 квартала
+        /// </summary>
+        public float Code_22003 { get; set;  }
+        
+        /// <summary>
+        /// Выручка - за последние 4 квартала
+        /// </summary>
+        public float Code_21103 { get; set;  }
     }
 }
