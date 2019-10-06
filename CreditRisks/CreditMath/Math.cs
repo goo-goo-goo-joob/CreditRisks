2c5e9a866be78860efcedaba51a8066858124236
asciishell <asciishell@yandex.ru>
2019-10-06 23:20:28 +0300

    Add math formulas

---
+++ CreditRisks/CreditMath/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CreditMath
+{
+    public class Math
+    {
+        public static float Winsorization(float value, float left, float right)
+        {
+            if (value > right)
+                return right;
+            if (value < left)
+                return left;
+            return value;
+        }
+
+        public static float MinMaxNorm(float value, float left, float right)
+        {
+            return (value - left) / (right - left);
+        }
+    }
+}
