e52e2ad832df7d1d42a17b1606c5e7dd125d6e0f
asciishell <asciishell@yandex.ru>
2019-12-18 01:39:58 +0300

    Popularity multiplot

--- metrics_library/profits.py
+++ metrics_library/profits.py
@@ -250,6 +250,8 @@ def plt_mcc(y_true: np.ndarray, y_score: np.ndarray,
 
 
 def plt_popularity(y_score: np.ndarray,
+                   ax=None,
+                   title=None,
                    thresholds=None,
                    yscale='linear',
                    progress_bar=False):
@@ -258,14 +260,15 @@ def plt_popularity(y_score: np.ndarray,
         thresholds = np.linspace(0, 1, 201)
     hist = []
     for i in iterator(range(1, len(thresholds))):
-        val = ((thresholds[i - 1] < y_score) & (y_score <= thresholds[i])).mean()
+        val = ((y_score <= thresholds[i])).mean()
         hist.append(val)
-    plt.figure(figsize=(7, 7), facecolor='w')
+    if ax is None:
+        plt.figure(figsize=(7, 7), facecolor='w')
     plt.plot(thresholds[1:], hist)
     plt.xlim([0, 1])
     plt.ylim([0, 1])
     # plt.yscale(yscale)
     plt.xlabel('Threshold')
-    plt.ylabel('Количество элементов в разбиении')
-    plt.title('Распределение вероятностей предсказания. Групп: {}'.format(len(thresholds) - 1))
+    plt.ylabel('Доля объектов выше порога')
+    plt.title('Распределение объектов по вероятностям' if title is None else title)
     plt.grid()
