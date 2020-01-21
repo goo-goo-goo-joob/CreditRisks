4f16f87dde08463feb0eaacff626581290a473e5
asciishell <asciishell@yandex.ru>
2020-01-22 00:46:58 +0300

    Add file handler

--- CreditRisks/CreditRisksRestAPI/Controllers/CalcController.cs
+++ CreditRisks/CreditRisksRestAPI/Controllers/CalcController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CreditRisksRestAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CreditRisksRestAPI.Controllers
@@ -17,29 +19,11 @@ namespace CreditRisksRestAPI.Controllers
             return new string[] {"value1", "value2"};
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromForm] CompanyFileMain value)
         {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            Console.WriteLine(value);
         }
     }
 }
