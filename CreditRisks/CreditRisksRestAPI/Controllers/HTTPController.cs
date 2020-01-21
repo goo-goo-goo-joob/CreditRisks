fb0a696685b5e2ff567de9231e05a7a14ee7fe75
asciishell <asciishell@yandex.ru>
2020-01-21 23:07:11 +0300

    Simple template

--- CreditRisks/CreditRisksRestAPI/Controllers/HTTPController.cs
+++ CreditRisks/CreditRisksRestAPI/Controllers/HTTPController.cs
@@ -1,15 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CreditRisksRestAPI.Controllers
 {
     [Route("")]
     [ApiController]
-    public class HttpController : ControllerBase
+    public class IndexController : ControllerBase
     {
         [HttpGet]
         public ActionResult<string> Get()
         {
-            return "value";
+
+            return Content(Template.Render.RenderPage("Index.html", "base.html"), "text/html", Encoding.UTF8);
+        }
+    }
+
+    [Route("CompanyFile")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Content(Template.Render.RenderPage("CompanyFile.html", "base.html"), "text/html", Encoding.UTF8);
         }
     }
 }
