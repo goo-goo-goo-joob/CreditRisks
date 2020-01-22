e93893af2028ab4af8ca513c41c24703905ad6d4
asciishell <asciishell@yandex.ru>
2020-01-22 23:14:50 +0300

    Fix building

--- CreditRisks/CreditRisksRestAPI/Controllers/CalcController.cs
+++ CreditRisks/CreditRisksRestAPI/Controllers/CalcController.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Calcservice;
-using CreditRisks.Models;
 using CreditRisksRestAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
