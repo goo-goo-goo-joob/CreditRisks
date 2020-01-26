10f85a2ab4bd2823842640c61e4f6370cbc600f0
asciishell <asciishell@yandex.ru>
2020-01-27 01:54:07 +0300

    Add catboost support

--- PythonBackend/calc_model/catB_model.py
+++ PythonBackend/calc_model/catB_model.py
@@ -1,28 +1,30 @@
 import io
 import zipfile
 
-import joblib
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import SGDClassifier
-from sklearn.preprocessing import StandardScaler
+from catboost import CatBoostClassifier
 
 from .abstract_model import AbstractModel
-from catboost import CatBoostClassifier
+
 
 class CatBModel(AbstractModel):
     cols = None
-    grB = CatBoostClassifier()
+    clf = CatBoostClassifier()
+    filename = 'catboost_cant_read_from_memory'
 
     def __init__(self, name: str, file_stream: io.BytesIO):
         super().__init__(name, file_stream)
         with zipfile.ZipFile(file_stream) as zfile:
-            with zfile.open('colsС.npy') as f:
+            with zfile.open('colsC.npy') as f:
                 self.cols = np.load(f, allow_pickle=True)
-            with zfile.open('model4_1') as f:
-                self.lr = grB.load_model(f)
+            with zfile.open('model4_1') as f, open(CatBModel.filename, 'wb') as cf:
+                cf.write(f.read())
+            self.clf.load_model(CatBModel.filename)
 
     def predict_proba(self, item: pd.DataFrame) -> float:
         obj = item[self.cols]
-        proba = self.lr.predict_proba(obj)[:, 1]
-        return proba[0]
+        proba = self.clf.predict_proba(obj)[:, 1]
+        if len(proba) == 1:
+            return proba[0]
+        return proba
