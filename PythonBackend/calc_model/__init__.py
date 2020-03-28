5c1af1da6fd69a43dbf6a2f94a3d0ceaaee96874
asciishell <asciishell@yandex.ru>
2020-03-28 22:07:40 +0300

    delete random model code

--- PythonBackend/calc_model/__init__.py
+++ PythonBackend/calc_model/__init__.py
@@ -1,5 +1,4 @@
 from .abstract_model import AbstractModel
 from .bank_model import BankModel
 from .catB_model import CatBModel
-from .random_model import RandomModel
 from .sgd_model import SGDModel
