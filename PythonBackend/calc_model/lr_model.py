620d69ba894a7dbc6d673366d2fbd5324aa247f0
asciishell <asciishell@yandex.ru>
2020-03-31 19:47:28 +0300

    fix pipeline

--- PythonBackend/calc_model/lr_model.py
+++ PythonBackend/calc_model/lr_model.py
@@ -27,74 +27,70 @@ class Winsorizator:
         self.fit(x).transform(x)
 
 
-methodCols = ['financialDebt', 'CreditLeverage', 'FinancialIndependence', 'DebtBurden',
-              'CoverageDebtWithAccumulatedProfit',
-              'ReturnAssetsNetProfit', 'ReturnAssetsOperatingProfit', 'OperatingMargin', 'NetProfitMargin',
-              'LiabilityCoverageOperatingProfit', 'OperatingProfitFinancialDebtRatio', 'FinancialDebtRevenueRatio',
-              'CurrentLiquidity', 'QuickLiquidity', 'InstantLiquidity', 'LevelOfOperatingAssets', 'turnoverDebtorDebt',
-              'turnoverReserves', 'turnoverCreditDebt', 'FinancialCycle', 'AssetTurnover']
+def add_features(x: pd.DataFrame):
+    method_cols = ['financialDebt', 'CreditLeverage', 'FinancialIndependence', 'DebtBurden',
+                   'CoverageDebtWithAccumulatedProfit',
+                   'ReturnAssetsNetProfit', 'ReturnAssetsOperatingProfit', 'OperatingMargin', 'NetProfitMargin',
+                   'LiabilityCoverageOperatingProfit', 'OperatingProfitFinancialDebtRatio', 'FinancialDebtRevenueRatio',
+                   'CurrentLiquidity', 'QuickLiquidity', 'InstantLiquidity', 'LevelOfOperatingAssets', 'turnoverDebtorDebt',
+                   'turnoverReserves', 'turnoverCreditDebt', 'FinancialCycle', 'AssetTurnover']
 
+    for year in [0, -1]:
+        x[f'year_{year}_okved2'] = x[f'year_{year}_okved'].str.extract(r'(^[0-9]+.[0-9]+)').fillna('__null__')
+        x[f'year_{year}_okved1'] = x[f'year_{year}_okved'].str.extract(r'(^[0-9]+)').fillna('__null__')
 
-def add_features(X: pd.DataFrame):
-    for year in ['-1', '0']:
-        y = '3' if year == '0' else '9'
-        z = '9' if year == '0' else '4'
-        X[f'year_{year}_financialDebt'] = X[f'year_0_1500{y}'] + X[f'year_0_1400{y}'] + X[f'year_0_1250{y}']
-        financial_debt = X[f'year_{year}_financialDebt']
-        X[f'year_{year}_CreditLeverage'] = X[f'year_0_1300{y}'] / X[f'year_0_1500{y}']
-        X[f'year_{year}_FinancialIndependence'] = X[f'year_0_1300{y}'] / X[f'year_0_1600{y}']
-        X[f'year_{year}_DebtBurden'] = financial_debt / X[f'year_0_1600{y}']
-        X[f'year_{year}_CoverageDebtWithAccumulatedProfit'] = X[f'year_0_1300{y}'] / financial_debt
-        X[f'year_{year}_ReturnAssetsNetProfit'] = X[f'year_0_2400{y}'] / X[f'year_0_1600{y}']
-        X[f'year_{year}_ReturnAssetsOperatingProfit'] = X[f'year_0_2200{y}'] / X[f'year_0_1600{y}']
-        X[f'year_{year}_OperatingMargin'] = X[f'year_0_2200{y}'] / pd.concat([X[f'year_0_2110{y}'], financial_debt],
-                                                                             axis=1).max(axis=1)
-        X[f'year_{year}_NetProfitMargin'] = X[f'year_0_2400{y}'] / pd.concat([X[f'year_0_2110{y}'], financial_debt],
-                                                                             axis=1).max(axis=1)  # impotant
-        X[f'year_{year}_LiabilityCoverageOperatingProfit'] = X[f'year_0_2200{y}'] / (
-                X[f'year_0_1400{y}'] + X[f'year_0_1500{y}'])
-        X[f'year_{year}_OperatingProfitFinancialDebtRatio'] = X[f'year_0_2200{y}'] / financial_debt
-        X[f'year_{year}_FinancialDebtRevenueRatio'] = financial_debt / X[f'year_0_2110{y}']  # impotant
-        X[f'year_{year}_CurrentLiquidity'] = X[f'year_0_1200{y}'] / X[f'year_0_1500{y}']
-        X[f'year_{year}_QuickLiquidity'] = (X[f'year_0_1200{y}'] - X[f'year_0_1210{y}']) / X[f'year_0_1500{y}']
-        X[f'year_{year}_InstantLiquidity'] = X[f'year_0_1250{y}'] / X[f'year_0_1500{y}']  # impotant
-        X[f'year_{year}_LevelOfOperatingAssets'] = (X[f'year_0_1210{y}'] + X[f'year_0_1230{y}'] - X[
-            f'year_0_1520{y}']) / X[f'year_0_2110{y}']
-        X[f'year_{year}_turnoverDebtorDebt'] = 365 * (X[f'year_0_1230{y}'] + X[f'year_{year}_1230{z}']) / (
-                2 * X[f'year_0_2110{y}'])
-        X[f'year_{year}_turnoverReserves'] = 365 * (X[f'year_0_1210{y}'] + X[f'year_{year}_1210{z}']) / (
-                2 * X[f'year_0_2110{y}'])
-        X[f'year_{year}_turnoverCreditDebt'] = 365 * (X[f'year_0_1520{y}'] + X[f'year_{year}_1520{z}']) / (
-                2 * X[f'year_0_2110{y}'])
-        X[f'year_{year}_FinancialCycle'] = X[f'year_{year}_turnoverDebtorDebt'] + X[f'year_{year}_turnoverReserves'] - \
-                                           X[f'year_{year}_turnoverCreditDebt']
-        X[f'year_{year}_AssetTurnover'] = X[f'year_0_2110{y}'] / X[f'year_0_1600{y}']
-        for col in methodCols:
-            X[f'year_{year}_{col}'].replace(np.nan, 0, inplace=True)
+        x[f'year_{year}_financialDebt'] = x[[f'year_{year}_1500', f'year_{year}_1400', f'year_{year}_1250']].sum(axis=1)
+        financial_debt = x[f'year_{year}_financialDebt']
+        x[f'year_{year}_CreditLeverage'] = x[f'year_{year}_1300'] / x[f'year_{year}_1500']
+        x[f'year_{year}_FinancialIndependence'] = x[f'year_{year}_1300'] / x[f'year_{year}_1600']
+        x[f'year_{year}_DebtBurden'] = financial_debt / x[f'year_{year}_1600']
+        x[f'year_{year}_CoverageDebtWithAccumulatedProfit'] = x[f'year_{year}_1300'] / financial_debt
+        x[f'year_{year}_ReturnAssetsNetProfit'] = x[f'year_{year}_2400'] / x[f'year_{year}_1600']
+        x[f'year_{year}_ReturnAssetsOperatingProfit'] = x[f'year_{year}_2200'] / x[f'year_{year}_1600']
+        x[f'year_{year}_OperatingMargin'] = x[f'year_{year}_2200'] / x[[f'year_{year}_2110', f'year_{year}_financialDebt']].max(axis=1)
+        x[f'year_{year}_NetProfitMargin'] = x[f'year_{year}_2400'] / x[[f'year_{year}_2110', f'year_{year}_financialDebt']].max(axis=1)
+        x[f'year_{year}_LiabilityCoverageOperatingProfit'] = x[f'year_{year}_2200'] / x[[f'year_{year}_1400', f'year_{year}_1500']].sum(axis=1)
+        x[f'year_{year}_OperatingProfitFinancialDebtRatio'] = x[f'year_{year}_2200'] / financial_debt
+        x[f'year_{year}_FinancialDebtRevenueRatio'] = financial_debt / x[f'year_{year}_2110']
+        x[f'year_{year}_CurrentLiquidity'] = x[f'year_{year}_1200'] / x[f'year_{year}_1500']
+        x[f'year_{year}_QuickLiquidity'] = (x[f'year_{year}_1200'] - x[f'year_{year}_1210']) / x[f'year_{year}_1500']
+        x[f'year_{year}_InstantLiquidity'] = x[f'year_{year}_1250'] / x[f'year_{year}_1500']
+        x[f'year_{year}_LevelOfOperatingAssets'] = (x[f'year_{year}_1210'] + x[f'year_{year}_1230'] - x[f'year_{year}_1520']) / x[f'year_{year}_2110']
+        x[f'year_{year}_turnoverDebtorDebt'] = 365 * (x[f'year_{year}_1230'] + x[f'year_{year - 1}_1230']) / (2 * x[f'year_{year}_2110'])
+        x[f'year_{year}_turnoverReserves'] = 365 * (x[f'year_{year}_1210'] + x[f'year_{year - 1}_1210']) / (2 * x[f'year_{year}_2110'])
+        x[f'year_{year}_turnoverCreditDebt'] = 365 * (x[f'year_{year}_1520'] + x[f'year_{year - 1}_1520']) / (2 * x[f'year_{year}_2110'])
+        x[f'year_{year}_FinancialCycle'] = x[f'year_{year}_turnoverDebtorDebt'] + x[f'year_{year}_turnoverReserves'] - x[f'year_{year}_turnoverCreditDebt']
+        x[f'year_{year}_AssetTurnover'] = x[f'year_{year}_2110'] / x[f'year_{year}_1600']
+        for col in method_cols:
+            x[f'year_{year}_{col}'].replace(np.inf, 1000000, inplace=True)
+            x[f'year_{year}_{col}'].replace(-np.inf, -1000000, inplace=True)
+            x[f'year_{year}_{col}'].replace(np.nan, 0, inplace=True)
 
 
 class LRModel(AbstractModel):
-    def __init__(self, name, plots, input_cols, cols, ce, wz, sc, lr):
+    def __init__(self, name, plots, cols, cat_cols, ce, wz, sc, lr):
         super().__init__(name, plots)
-        assert isinstance(input_cols, np.ndarray)
         assert isinstance(cols, np.ndarray)
+        assert isinstance(cat_cols, np.ndarray)
         assert isinstance(ce, category_encoders.CatBoostEncoder)
         assert isinstance(wz, Winsorizator)
         assert isinstance(sc, StandardScaler)
         assert isinstance(lr, LogisticRegression)
-        self.input_cols = input_cols
         self.cols = cols
+        self.cat_cols = cat_cols
+        self.float_cols = sorted(list(set(self.cols) - set(self.cat_cols)))
         self.ce = ce
         self.wz = wz
         self.sc = sc
         self.lr = lr
 
     def predict_proba(self, item: pd.DataFrame) -> Union[float, np.ndarray]:
-        item = item[self.input_cols]
         add_features(item)
         item = item[self.cols]
-        item = self.ce.transform(item)
-        self.wz.transform(item)
+        item_cat = self.ce.transform(item[self.cat_cols])
+        item_float = item[self.float_cols].copy()
+        self.wz.transform(item_float)
+        item = np.hstack([item_float, item_cat])
         item = self.sc.transform(item)
         proba = self.lr.predict_proba(item)[:, 1]
         if len(proba) == 1:
