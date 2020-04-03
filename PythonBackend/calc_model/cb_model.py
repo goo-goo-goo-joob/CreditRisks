25bbb974f5bdbccc2fe8df83120f0f980beb1d1e
asciishell <asciishell@yandex.ru>
2020-04-03 14:02:31 +0300

    Add cat cols

--- PythonBackend/calc_model/cb_model.py
+++ PythonBackend/calc_model/cb_model.py
@@ -6,11 +6,13 @@ from .abstract_model import AbstractModel
 
 
 class CatBoostModel(AbstractModel):
-    def __init__(self, name, plots, cols, cb):
+    def __init__(self, name, plots, cols, cat_cols, cb):
         super().__init__(name, plots)
         assert isinstance(cols, np.ndarray)
+        assert isinstance(cat_cols, np.ndarray)
         assert isinstance(cb, CatBoostClassifier)
         self.cols = cols
+        self.cat_cols = cat_cols
         self.model = cb
 
     def predict_proba(self, item: pd.DataFrame) -> float:
