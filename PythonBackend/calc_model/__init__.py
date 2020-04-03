46e6df15f6ff70d9732f3701725ed873c9c0b233
asciishell <asciishell@yandex.ru>
2020-04-03 13:46:45 +0300

    Update catboost

--- PythonBackend/calc_model/__init__.py
+++ PythonBackend/calc_model/__init__.py
@@ -1,4 +1,4 @@
 from .abstract_model import AbstractModel
 from .bank_model import BankModel
-from .catB_model import CatBModel
+from .cb_model import CatBModel
 from .sgd_model import SGDModel
---
+++ PythonBackend/calc_model/cb_model.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from catboost import CatBoostClassifier
+
+from .abstract_model import AbstractModel
+
+
+class CatBModel(AbstractModel):
+    def __init__(self, name, plots, cols, cb):
+        super().__init__(name, plots)
+        assert isinstance(cols, np.ndarray)
+        assert isinstance(cb, CatBoostClassifier)
+        self.cols = cols
+        self.model = cb
+
+    def predict_proba(self, item: pd.DataFrame) -> float:
+        obj = item[self.cols]
+        proba = self.model.predict_proba(obj)[:, 1]
+        if len(proba) == 1:
+            return proba[0]
+        return proba
