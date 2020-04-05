1685956d9c6211bb39232dacdc84b7e987ae8e18
asciishell <asciishell@yandex.ru>
2020-04-05 13:49:44 +0300

    update front page

--- PythonBackend/calculation_handler.py
+++ PythonBackend/calculation_handler.py
@@ -7,8 +7,8 @@ import pandas as pd
 import feature_impact
 from feature_generation import add_features
 
-RESULT_DTYPES = {'DesireToInvest': np.int32,
-                 'IndustryRating': np.int32,
+RESULT_DTYPES = {'BusinessModelRisk': np.int32,
+                 'DesireToInvest': np.int32,
                  'ManagementShareholdersConflict': np.int32,
                  'NegativeShareholders': np.int32,
                  'OwnFundsTransaction': np.int32,
@@ -101,7 +101,7 @@ def dict_to_df(data):
             res_types[key] = dtype
         if dtype == np.float:
             df[key] = df[key].str.replace(',', '.')
-    return df.astype(dtype=res_types)
+    return df[res_types.keys()].astype(dtype=res_types)
 
 
 class CalcHandler:
