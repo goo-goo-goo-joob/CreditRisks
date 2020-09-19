3573506d2da5b433d02c2cfc489132bb30a20301
asciishell <asciishell@yandex.ru>
2020-09-19 12:54:11 +0300

    remove server multiprocessing

--- PythonBackend/server.py
+++ PythonBackend/server.py
@@ -1,28 +1,18 @@
-import contextlib
 import datetime
 import logging
-import multiprocessing
 import os
-import socket
 import sys
-import time
 from concurrent import futures
-
-import grpc
 
 import calc_service_pb2
 import calc_service_pb2_grpc
+import grpc
 from calculation_handler import CalcHandler
 
 LOGGER = logging.getLogger(__name__)
 LISTEN_PORT = os.getenv("LISTEN_PORT", 9000)
 ONE_DAY = datetime.timedelta(days=1)
-if sys.platform.startswith('linux'):
-    _PROCESS_COUNT = int(os.getenv("PROCESS_COUNT", multiprocessing.cpu_count()))
-elif sys.platform.startswith('win32'):
-    _PROCESS_COUNT = 1
-else:
-    raise Exception("Unsupported OS")
+_PROCESS_COUNT = 1  # some bug in grpc
 _MODEL_PATH = os.getenv("MODEL_PATH")
 
 
@@ -40,55 +30,18 @@ class Handler(calc_service_pb2_grpc.CalcServiceServicer):
         return calc_service_pb2.ModelInfoReply(Result=result)
 
     def GetImpact(self, request, context):
-        image = self.calc_handler.get_impact(request.ModelName, request.Data, request.Feature, request.Head, request.Tail)
+        image = self.calc_handler.get_impact(request.ModelName, request.Data, request.Feature, request.Head,
+                                             request.Tail)
         return calc_service_pb2.ImpactReply(Image=image)
 
 
-def _wait_forever(server):
-    try:
-        while True:
-            time.sleep(ONE_DAY.total_seconds())
-    except KeyboardInterrupt:
-        server.stop(None)
-
-
-def _run_server(bind_address):
-    """Start a server in a subprocess."""
-    LOGGER.info('Starting new server.')
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=_PROCESS_COUNT), options=(('grpc.so_reuseport', 1),))
-    calc_service_pb2_grpc.add_CalcServiceServicer_to_server(Handler(), server)
-    server.add_insecure_port(bind_address)
-    server.start()
-    _wait_forever(server)
-
-
-@contextlib.contextmanager
-def _reserve_port():
-    """Find and reserve a port for all subprocesses to use."""
-    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    if sys.platform.startswith('linux'):
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        if sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT) == 0:
-            raise RuntimeError("Failed to set SO_REUSEPORT.")
-    sock.bind(('', LISTEN_PORT))
-    try:
-        yield sock.getsockname()[1]
-    finally:
-        sock.close()
-
-
 def main():
-    with _reserve_port() as port:
-        bind_address = '[::]:{}'.format(port)
-        LOGGER.info("Binding to '%s'", bind_address)
-        sys.stdout.flush()
-        workers = []
-        for _ in range(_PROCESS_COUNT):
-            worker = multiprocessing.Process(target=_run_server, args=(bind_address,))
-            worker.start()
-            workers.append(worker)
-        for worker in workers:
-            worker.join()
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=_PROCESS_COUNT))
+    calc_service_pb2_grpc.add_CalcServiceServicer_to_server(Handler(), server)
+    server.add_insecure_port('[::]:{}'.format(LISTEN_PORT))
+    server.start()
+    LOGGER.info('server started')
+    server.wait_for_termination()
 
 
 if __name__ == '__main__':
